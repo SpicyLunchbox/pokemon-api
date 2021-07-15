6075869733d2076cb48356c709b5cd9fcc25392c
SpicyLunchbox <westonwoodard28@gmail.com>
2021-07-14 17:18:19 -0700

    rough

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import PokemonCard from './PokemonCard';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([])
+
+  function generateRange() {
+    let range = [];
+    for (let i = 1; i < 152; i++) {
+      range.push(i);
+    }
+    return range;
+  }
+
+  useEffect(() => {
+    generateRange().forEach(num => {
+      console.log(num)
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`)
+        .then(res => {
+          pokemonData.push(res.data)
+          
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }) 
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Pokemon API</h1>
+        <h2>Gotta catch em all</h2>
       </header>
+      {pokemonData.map(pokemon => {
+        return <PokemonCard pokemon={pokemon}/>
+      })}
+      <footer className="App-footer">
+
+      </footer>
     </div>
   );
 }
